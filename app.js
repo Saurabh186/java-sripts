3e6e27375647f16b0fb3cfd6d7e28c467e710f0f
Saurabh186 <sawrabhsharma1234@gmail.com>
2024-04-08 11:55:11 -0700

    js script add

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-const item = document.querySelector(#item)
-const toDoBox = document.querySelector(#to-do-box)
+const item = document.querySelector("#item")
+const toDoBox = document.querySelector("#to-do-box")
 
-item.add
+item.addEventListener(
+    "keyup",
+    function(event) {
+        if (event.key == "Enter") {
+            addToDo(this.value)
+            this.value = ""
+        }
+    }
+)
+
+const addToDo = (item) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = ` ${item} <span class="fw-bolder text-white">X </span>`;
+    listItem.addEventListener(
+        "click",
+        function() {
+            this.classList.toggle("done");
+        }
+    )
+    listItem.querySelector("i").addEventListener(
+        "click",
+        function(){
+            listItem.remove()
+        }
+    )
+    toDoBox.appendChild(listItem)
+}
